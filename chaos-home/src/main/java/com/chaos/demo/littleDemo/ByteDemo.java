89d4b2122b12f7d594f18edc237fc098a32e048b
liaopeng <liaopeng@hualala.com>
2020-05-09 17:14:45 +0800

    127+1=-128

---
+++ chaos-home/src/main/java/com/chaos/demo/littleDemo/ByteDemo.java
@@ -0,0 +1,16 @@
+package com.chaos.demo.littleDemo;
+
+/**
+ * @program: chaos
+ * * @description:
+ * * @author: liaopeng
+ * * @create: 2020-05-09 15:47
+ **/
+public class ByteDemo {
+    public static void main(String[] args) {
+        byte m =127;
+        m++;
+        System.out.println(m);
+
+    }
+}
